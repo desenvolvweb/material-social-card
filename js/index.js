f4f0baf8686e054203067efacffce182407791f5
Tárcio Zemel <tarciozemel@gmail.com>
2020-04-04 15:06:47 -0300

    A little refinement

--- js/index.js
+++ js/index.js
@@ -1,42 +1,29 @@
 const $cards = document.querySelectorAll(".js-card");
 const $menu = document.querySelector(".js-menu");
-const $menuToggles = $menu.querySelectorAll(".js-section-toggle");
+const sectionToggleClass = "js-section-toggle";
+const $menuToggles = $menu.querySelectorAll(`.${sectionToggleClass}`);
 
-$menu.addEventListener("click", e => {
-  e.preventDefault();
-
-  if (isSectionToggle(e.target)) {
+$menu.addEventListener("click", (e) => {
+  if (e.target.classList.contains(sectionToggleClass)) {
     activateCard(e.target.parentElement);
     markAsActiveMenuItem(e.target);
   }
 });
 
-function isSectionToggle(el) {
-  return el.classList.contains("js-section-toggle");
-}
-
 function activateCard(el) {
-  const socialnetworkidentifier = findSocialNetworkIdentifier(el);
+  const socialnetwork = el.dataset.socialNetwork;
 
-  $cards.forEach($card => {
+  $cards.forEach(($card) => {
     $card.classList.remove("is-active");
 
-    if ($card.classList.value.includes(socialnetworkidentifier)) {
+    if ($card.classList.value.includes(socialnetwork)) {
       $card.classList.add("is-active");
     }
   });
 }
 
-function findSocialNetworkIdentifier(el) {
-  for (const className of el.classList) {
-    if (className.includes("--")) {
-      return className.split("--")[1];
-    }
-  }
-}
-
 function markAsActiveMenuItem(el) {
-  $menuToggles.forEach($item => {
+  $menuToggles.forEach(($item) => {
     $item.classList.remove("is-active");
   });
 
